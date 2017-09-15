c4a67c1bbd242de9e326ca378ff9dc3be1e05c71
Cristian Galvan <navlag@MacBook-Pro.local>
2017-09-14 19:19:29 -0700

    added buttons and labels to viewcontroller, added a second view controller, and a new viewcontroller file

--- Homework1/ViewController.swift
+++ Homework1/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tunerButton: UIButton!
+    @IBOutlet var tunerImage: UIImageView!
+    @IBOutlet var muscleButton: UIButton!
+    @IBOutlet var muscleImage: UIImageView!
+    @IBOutlet var exoticButton: UIButton!
+    @IBOutlet var exoticImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        elementToggle(toggle: false);
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func elementToggle(toggle: Bool) {
+        tunerButton.isHidden = toggle;
+        tunerImage.isHidden = toggle;
+        muscleButton.isHidden = toggle;
+        muscleImage.isHidden = toggle;
+        exoticButton.isHidden = toggle;
+        exoticImage.isHidden = toggle;
+        titleLabel.isHidden = toggle;
+    }
+    
+    @IBAction func tunerToggle(_ sender: Any) {
+        elementToggle(toggle: true);
 
+    }
+    @IBAction func muscleToggle(_ sender: Any) {
+        elementToggle(toggle: true);
+
+    }
+    @IBAction func exoticToggle(_ sender: Any) {
+        elementToggle(toggle: true);
+    }
 }
 
